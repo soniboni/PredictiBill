2e26ec5120a007a4f658e2efab9996c798dd948e
Soni <112708293+soniboni@users.noreply.github.com>
2025-05-01 21:58:49 +0800

    Onboarding 1-3 Pages done (front-end only)

---
+++ app/src/main/java/com/example/predictibill/OnboardingTwo.java
@@ -0,0 +1,12 @@
+package com.example.predictibill;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OnboardingTwo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.onboarding_two); // link to XML layout
+    }
+}
