c9b06c09f09eac7d57f913eb3cfd5dbce0145140
Soni <112708293+soniboni@users.noreply.github.com>
2025-05-02 21:28:36 +0800

    SIgn Up Layout is okay, but needs to work on the constraint

--- app/src/main/java/com/example/predictibill/OnboardingThree.java
+++ app/src/main/java/com/example/predictibill/OnboardingThree.java
@@ -1,5 +1,9 @@
 package com.example.predictibill;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OnboardingThree extends AppCompatActivity {
@@ -7,5 +11,15 @@ public class OnboardingThree extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.onboarding_three);
+
+        Button getStartedButton = findViewById(R.id.getStarted_button);
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OnboardingThree.this, SignUp.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out); // Optional
+            }
+        });
     }
 }
--- app/src/main/java/com/example/predictibill/SignUp.java
+++ app/src/main/java/com/example/predictibill/SignUp.java
@@ -1,4 +1,12 @@
 package com.example.predictibill;
 
-public class SignUp {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up); // Links to res/layout/signup.xml
+    }
 }
