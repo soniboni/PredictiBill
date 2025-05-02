193bb1d2d34224a05e06ef77aa3ee7030ba5d254
jajai <marcialjaira7@gmail.com>
2025-05-02 22:47:04 +0800

    Changes on Subscription page added AddSubscription Fragment added a few on the res folder

---
+++ app/src/main/java/com/example/predictibill/ui/subscriptions/AddSubscriptionFragment.java
@@ -0,0 +1,12 @@
+package com.example.predictibill.ui.subscriptions;
+
+import androidx.fragment.app.Fragment;
+import com.example.predictibill.R;
+
+public class AddSubscriptionFragment extends Fragment {
+    public AddSubscriptionFragment() {
+        super(R.layout.fragments_add_subscriptions);
+    }
+}
+
+
