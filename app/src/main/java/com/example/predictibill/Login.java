1999966c3c06bf9632c3e5e47a915cff5ed75da0
Soni <112708293+soniboni@users.noreply.github.com>
2025-05-15 06:14:05 +0800

    Login page layout done & Clickable TextView functioning
    Sign Up or Login footers (TextView) are now clickable and successfully transfers from one page to another

---
+++ app/src/main/java/com/example/predictibill/Login.java
@@ -0,0 +1,28 @@
+package com.example.predictibill;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        TextView signUpFooterTxt = findViewById(R.id.signUpFooter_txt);
+        signUpFooterTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out); // Optional animation
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/predictibill/SignUp.java
+++ app/src/main/java/com/example/predictibill/SignUp.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp extends AppCompatActivity {
@@ -13,12 +15,23 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.sign_up); // Links to res/layout/sign_up.xml
 
         Button signUpButton = findViewById(R.id.signUp_button);
+        TextView loginFooterTxt = findViewById(R.id.loginFooter_txt);
+
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignUp.this, AccountCreated.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.fade_in, R.anim.fade_out); // Optional transition
+            }
+        });
+
+        loginFooterTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out); // Optional animation
             }
         });
     }
