76870b5080f294ba61e3db3d074764c220ed2cfa
jajai <marcialjaira7@gmail.com>
2025-05-20 05:37:23 +0800

    Working "add subscription" button and "back" button, Changed the "filter by" buttons into chip category, Changed the color of the nav graph,Started on the "Add a Subscription Layout"

--- app/src/main/java/com/example/predictibill/ui/subscriptions/AddSubscriptionFragment.java
+++ app/src/main/java/com/example/predictibill/ui/subscriptions/AddSubscriptionFragment.java
@@ -1,12 +1,29 @@
 package com.example.predictibill.ui.subscriptions;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
 import com.example.predictibill.R;
 
 public class AddSubscriptionFragment extends Fragment {
     public AddSubscriptionFragment() {
         super(R.layout.fragments_add_subscriptions);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageButton backButton = view.findViewById(R.id.back_button);
+        backButton.setOnClickListener(v -> {
+            // Use NavController if using Jetpack Navigation
+            NavHostFragment.findNavController(this).navigateUp();
+
+        });
+    }
 }
-
-
