5bace9e78b5d3028c0f63a653931a7845b14cd01
Janna <142386301+jannarchive@users.noreply.github.com>
2025-05-26 21:09:22 +0800

    Added functionality to buttons in Subscription Details (Upcoming)
    Ang wala na lang:
    1. Navigation from Subscription Details (Upcoming) to Edit Information Fragment
    2. Logic para sa pag delete ng subscription

--- app/src/main/java/com/example/predictibill/ui/subscriptions/SubscriptionDetailsUpcomingFragment.java
+++ app/src/main/java/com/example/predictibill/ui/subscriptions/SubscriptionDetailsUpcomingFragment.java
@@ -20,7 +20,9 @@ import com.example.predictibill.R;
 public class SubscriptionDetailsUpcomingFragment extends Fragment {
 
     Dialog dialog;
-    ImageButton cancel_btn, confirm_btn;
+    Button mark_as_paid_cancel_btn, confirm_btn,
+            cancel_subs_btn, keep_tracking_btn,
+            delete_permanently_btn, delete_subs_cancel_btn;
 
     public SubscriptionDetailsUpcomingFragment() {
         // Required empty public constructor
@@ -49,8 +51,18 @@ public class SubscriptionDetailsUpcomingFragment extends Fragment {
 
         Button markAsPaid = view.findViewById(R.id.upcomingsubs_details_mark_as_paid_btn);
         markAsPaid.setOnClickListener(v -> showMarkAsPaidDialog());
+
+        Button cancelSubs = view.findViewById(R.id.upcomingsubs_details_cancel_subs_btn);
+        cancelSubs.setOnClickListener(v -> showCancelSubsDialog());
+
+        // lagay pa here yung for edit info
+
+        Button deleteSubs = view.findViewById(R.id.upcomingsubs_details_delete_subs_btn);
+        deleteSubs.setOnClickListener(v -> showDeleteSubsDialog());
     }
 
+
+    // ---------- TO SHOW MARK AS PAID DIALOG BOX
     private void showMarkAsPaidDialog() {
         if (getContext() == null) return;
 
@@ -64,15 +76,15 @@ public class SubscriptionDetailsUpcomingFragment extends Fragment {
             dialog.getWindow().setBackgroundDrawable(dialog_box_bg);
         }
 
-        dialog.setCancelable(true);
+        dialog.setCancelable(false);
 
         // Initialize buttons
-        cancel_btn = dialog.findViewById(R.id.cancel_btn);
+        mark_as_paid_cancel_btn = dialog.findViewById(R.id.mark_as_paid_cancel_btn);
         confirm_btn = dialog.findViewById(R.id.confirm_btn);
 
-        if (cancel_btn != null) {
+        if (mark_as_paid_cancel_btn != null) {
             //Dismiss the dialog box when the cancel button is clicked
-            cancel_btn.setOnClickListener(v -> dialog.dismiss());
+            mark_as_paid_cancel_btn.setOnClickListener(v -> dialog.dismiss());
         }
 
         if (confirm_btn != null) {
@@ -81,6 +93,81 @@ public class SubscriptionDetailsUpcomingFragment extends Fragment {
                 NavHostFragment.findNavController(this)
                         .navigate(R.id.action_subscriptionDetailsUpcoming_to_subscriptionDetailsPaid);
             });
+        }
+
+        // Show the dialog
+        dialog.show();
+    }
+
+
+    // ---------- TO SHOW CANCEL SUBSCRIPTION DIALOG BOX
+    private void showCancelSubsDialog() {
+        if (getContext() == null) return;
+
+        dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.cancel_subscription_dialog_box);
+
+        if (dialog.getWindow() != null) {
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            Drawable dialog_box_bg = ContextCompat.getDrawable(getContext(), R.drawable.dialog_box_bg);
+            dialog.getWindow().setBackgroundDrawable(dialog_box_bg);
+        }
+
+        dialog.setCancelable(false);
+
+        // Initialize buttons
+        cancel_subs_btn = dialog.findViewById(R.id.cancel_subs_btn);
+        keep_tracking_btn = dialog.findViewById(R.id.keep_tracking_btn);
+
+        if (cancel_subs_btn != null) {
+            cancel_subs_btn.setOnClickListener(v -> { dialog.dismiss();
+                // Navigate to cancelled subscription details
+                NavHostFragment.findNavController(this)
+                        .navigate(R.id.action_subscriptionDetailsUpcoming_to_subscriptionDetailsCancelled);
+            });
+        }
+
+        if (keep_tracking_btn != null) {
+            //Dismiss the dialog box when the keep tracking button is clicked
+            keep_tracking_btn.setOnClickListener(v -> dialog.dismiss());
+        }
+
+        // Show the dialog
+        dialog.show();
+    }
+
+
+    // ---------- TO SHOW DELETE SUBSCRIPTION DIALOG BOX
+    private void showDeleteSubsDialog() {
+        if (getContext() == null) return;
+
+        dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.delete_subscription_dialog_box);
+
+        if (dialog.getWindow() != null) {
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            Drawable dialog_box_bg = ContextCompat.getDrawable(getContext(), R.drawable.dialog_box_bg);
+            dialog.getWindow().setBackgroundDrawable(dialog_box_bg);
+        }
+
+        dialog.setCancelable(false);
+
+        // Initialize buttons
+        delete_permanently_btn = dialog.findViewById(R.id.delete_permanently_btn);
+        delete_subs_cancel_btn = dialog.findViewById(R.id.delete_subs_cancel_btn);
+
+        if (delete_permanently_btn != null) {
+            delete_permanently_btn.setOnClickListener(v -> { dialog.dismiss();
+
+                // insert here the logic for deletion of subscription/s
+            });
+        }
+
+        if (delete_subs_cancel_btn != null) {
+            //Dismiss the dialog box when the cancel button is clicked
+            delete_subs_cancel_btn.setOnClickListener(v -> dialog.dismiss());
         }
 
         // Show the dialog
